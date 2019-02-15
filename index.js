62b5fa92396a893d0b41cd3144868841478de479
Christina Lan <christina.lan13@gmail.com>
2019-02-15 16:02:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var itemPrice = Math.floor(Math.random()*100);
-   cart.push({[item] : itemPrice});
+ var price = Math.floor(Math.random()*100);
+   cart.push({[item] : price});
    console.log(`${item} has been added to your cart.`);
    return cart
  }
